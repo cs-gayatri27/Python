59e263007c55503d4bb4691e8791e52c5fe5d9fd
cs-gayatri27 <gayatri23012042@iimscollege.edu.np>
2025-12-23 20:31:21 +0545

    module and oop concept

---
+++ module.py
@@ -0,0 +1,15 @@
+"""
+
+Modules - single python file
+Packages - folder which contains multiple python files (modules)
+Built-in libraries - libraries which don't need to be installed. math, random, datetime
+Standard Libraries - libraries that needs to be installed for using them. numpy, pandas, matplotlib, seaborn
+pip - package installer for python
+
+Virtual Environments - it prevents package version conflict, portable , improves installation process, it allows to test different versions of libraries
+python -m venv website  -> to make virtual environment
+website\Scripts\activate   -> to activate virtual environment
+pip install [libraries_name]
+
+deactivate   -> to deactivate virtual environment
+"""
---
+++ oopIntro.py
@@ -0,0 +1,34 @@
+"""
+OOP- Object Oriented Programming
+Uses - 
+1. Clean and clear Structure
+2. Reuse of codes
+3. Easy to maintain
+4. Easy to debug
+5. DRY concept (Dont repeat yourself)
+6. Application development
+We use class keyword while creating the class.
+
+Class: collections of objects
+Object: is used to access the codes/functions/logics of class
+"""
+class calculation:
+    name = "Ram"
+    
+obj1 = calculation()
+print(obj1.name)
+obj2 = calculation()
+obj3 = calculation()
+print(obj2.name)
+print(obj3.name)
+# del obj1
+# print(obj1.name)
+
+
+# init method
+# self
+# multiple parameter
+# accessing properties using self
+# properties 
+
+
